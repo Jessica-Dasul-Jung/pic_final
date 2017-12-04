1630c419511320083656c5e2a689b3f5c7b51f64
Jessica Jung <jessicaj1998@hotmail.com>
2017-12-03 23:48:47 -0800

    re-added bullet class

--- Documents/Plane_Game/bullet.cpp
+++ Documents/Plane_Game/bullet.cpp
@@ -2,5 +2,18 @@
 
 Bullet::Bullet()
 {
+    this->setRect(0, 0, 10, 50);
+    m_timer = new QTimer();
+    connect (m_timer, SIGNAL(timeout()), this, SLOT(move()));
+    m_timer->start(50);
+}
 
+void Bullet::move()
+{
+    this->setPos(x(), y() - 10);
+    if (this->pos().y() + rect().height() < 0)
+    {
+        scene()->removeItem(this);
+        delete this;
+    }
 }
--- Documents/Plane_Game/bullet.h
+++ Documents/Plane_Game/bullet.h
@@ -1,11 +1,18 @@
 #ifndef BULLET_H
 #define BULLET_H
 
+#include "globals.h"
 
-class Bullet
+class Bullet: public QObject, public QGraphicsRectItem
 {
 public:
     Bullet();
+    Q_OBJECT
+
+public slots:
+    void move();
+private:
+    QTimer* m_timer;
 };
 
 #endif // BULLET_H
