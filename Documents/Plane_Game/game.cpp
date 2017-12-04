32f5d326ff7831b5c52d4bb68ef538768d99c6e5
Jessica Jung <jessicaj1998@hotmail.com>
2017-12-03 21:44:50 -0800

    game implementation

--- Documents/Plane_Game/game.cpp
+++ Documents/Plane_Game/game.cpp
@@ -1,6 +1,14 @@
 #include "game.h"
+#include "player.h"
 
 Game::Game()
 {
+    m_gamecount = 0;
+    m_level = 0;
 
+    m_player = new Player();
+    m_player->setRect(0, 0, 100, 100);
+    this->addItem(m_player);
+    m_player->setFlag(QGraphicsItem::ItemIsFocusable);
+    m_player->setFocus();
 }
--- Documents/Plane_Game/game.h
+++ Documents/Plane_Game/game.h
@@ -1,11 +1,17 @@
 #ifndef GAME_H
 #define GAME_H
+#include "globals.h"
 
-
-class Game
+class Player;
+class Game: public QGraphicsScene //or sth else?
 {
 public:
     Game();
+
+private:
+    int m_gamecount;
+    int m_level;
+    Player* m_player;
 };
 
 #endif // GAME_H
