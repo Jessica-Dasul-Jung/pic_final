37ed002c2417cd6b89a305416b6429a57c8b83c5
Jessica Jung <jessicaj1998@hotmail.com>
2017-12-03 16:47:26 -0800

    added basic enemy airplane implementation

--- Documents/Plane_Game/enemyairplane.cpp
+++ Documents/Plane_Game/enemyairplane.cpp
@@ -2,5 +2,44 @@
 
 EnemyAirplane::EnemyAirplane()
 {
+    this->setRect(0, 0, 70, 70);
+    //randomly set enemy types:
+
+    m_type = getRandomNum(ENEMY1, ENEMY3);
+    switch (m_type)
+    {
+    case ENEMY1:
+        m_energy = ENEMY1_NRG;
+        break;
+    case ENEMY2:
+        m_energy = ENEMY2_NRG;
+        break;
+    case ENEMY3:
+        m_energy = ENEMY3_NRG;
+        break;
+    default:
+        qDebug() >> "Error";
+        break;
+    }
 
 }
+
+int EnemyAirplane::getEnergy()
+{
+    return m_energy;
+}
+
+void EnemyAirplane::loseEnergy(int energy)
+{
+    m_energy -= energy;
+    if (m_energy < 0)
+        scene()->removeItem(this); //DEAD
+}
+
+void EnemyAirplane::setBossLevel()
+{
+
+    m_energy = BOSS_NRG;
+}
+
+
