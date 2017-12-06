c887fe01e2126a209044bb29f9f4d6f6aad7e507
Jessica Jung <jessicaj1998@hotmail.com>
2017-12-05 16:12:22 -0800

    fixed game implementation: changed it so enemyairplane is produced consistently

--- Documents/Plane_Game/game.cpp
+++ Documents/Plane_Game/game.cpp
@@ -1,6 +1,9 @@
 #include "game.h"
 #include "player.h"
+#include "enemyairplane.h"
+#include <QDebug>
 
+#define INTERVAL 100;
 Game::Game()
 {
     m_gamecount = 0;
@@ -8,7 +11,24 @@ Game::Game()
 
     m_player = new Player();
     m_player->setRect(0, 0, 100, 100);
+    m_player->setPos(WIN_WIDTH/2 - 50, WIN_HEIGHT - m_player->rect().height());
+
     this->addItem(m_player);
     m_player->setFlag(QGraphicsItem::ItemIsFocusable);
     m_player->setFocus();
+
+    m_timer = new QTimer();
+    QObject::connect (m_timer, SIGNAL(timeout()), this, SLOT(produceEnemy()));
+    m_timer->start(100);
+}
+
+void Game::produceEnemy()
+{
+    qDebug() << "produce Enemy";
+    for (int i = 0; i < ENEMY_NUM; i++)
+    {
+        qDebug() << "enemy produced";
+        EnemyAirplane* enemy = new EnemyAirplane();
+        this->addItem(enemy);
+    }
 }
--- Documents/Plane_Game/game.h
+++ Documents/Plane_Game/game.h
@@ -1,17 +1,25 @@
 #ifndef GAME_H
 #define GAME_H
 #include "globals.h"
+#include <QGraphicsScene>
 
 class Player;
+class EnemyAirplane;
 class Game: public QGraphicsScene //or sth else?
 {
 public:
     Game();
+    Q_OBJECT
+
+public slots:
+    void produceEnemy();
 
 private:
     int m_gamecount;
     int m_level;
     Player* m_player;
+//    EnemyAirplane* m_enemy[ENEMY_NUM];
+    QTimer* m_timer;
 };
 
 #endif // GAME_H
